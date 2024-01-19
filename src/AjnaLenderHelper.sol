61263a6e341ac13d27a8397a9bb5fc90fd7d4e0c
Ed Noepel <ed@noepel.net>
2024-01-19 11:57:55 -0500

    limit moveQuoteToken amount by the moveable amount, doco

--- src/AjnaLenderHelper.sol
+++ src/AjnaLenderHelper.sol
@@ -2,13 +2,25 @@
 pragma solidity ^0.8.13;
 
 import "@ajna-core/interfaces/pool/IPool.sol";
+import { _priceAt }  from '@ajna-core/libraries/helpers/PoolHelper.sol';
+import { Buckets }   from '@ajna-core/libraries/internal/Buckets.sol';
 import { Maths }     from "@ajna-core/libraries/internal/Maths.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 }    from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Math }      from '@openzeppelin/contracts/utils/math/Math.sol';
 
 contract AjnaLenderHelper {
     using SafeERC20 for IERC20;
 
+    /**
+     *  @notice Called by lenders to deposit into specified price bucket with protection against exchange rate manipulation.
+     *  @param  pool_        Address of the pool in which quote token shall be added.
+     *  @param  maxAmount_   The maximum amount of quote token lender wishes to add (`WAD` precision).
+     *  @param  index_       The index of the bucket to which the quote tokens will be added.
+     *  @param  expiry_      Timestamp after which this transaction will revert, preventing inclusion in a block with unfavorable price.
+     *  @return bucketLP_    The amount of `LP` changed for the added quote tokens (`WAD` precision).
+     *  @return addedAmount_ The amount of quote token added (`WAD` precision).
+     */
     function addQuoteToken(
         address pool_,
         uint256 maxAmount_,
@@ -35,6 +47,17 @@ contract AjnaLenderHelper {
         pool.transferLP(address(this), msg.sender, buckets);
     }
 
+    /**
+     *  @notice Called by lenders to move an amount of credit from a specified price bucket to another specified price bucket with protection against exchange rate manipulation.
+     *  @param  pool_         Address of the pool in which liquidity shall be moved.
+     *  @param  maxAmount_    The maximum amount of quote token to be moved by a lender (`WAD` precision).
+     *  @param  fromIndex_    The bucket index from which the quote tokens will be removed.
+     *  @param  toIndex_      The bucket index to which the quote tokens will be added.
+     *  @param  expiry_       Timestamp after which this transaction will revert, preventing inclusion in a block with unfavorable price.
+     *  @return fromBucketLP_ The amount of `LP` moved out from bucket (`WAD` precision).
+     *  @return toBucketLP_   The amount of `LP` moved to destination bucket (`WAD` precision).
+     *  @return movedAmount_  The amount of quote token moved (`WAD` precision).
+     */
     function moveQuoteToken(
         address pool_,
         uint256 maxAmount_,
@@ -44,6 +67,11 @@ contract AjnaLenderHelper {
     ) external returns (uint256 fromBucketRedeemedLP_, uint256 toBucketAwardedLP_, uint256 movedAmount_) {
         uint256 amount = maxAmount_;
         IPool pool = IPool(pool_);
+
+        // limit the move amount based on deposit available for lender to withdraw
+        pool.updateInterest();
+        (uint256 lenderLP, ) = pool.lenderInfo(fromIndex_, address(msg.sender));
+        amount = Maths.min(amount, _lpToQuoteToken(fromIndex_, lenderLP, pool));
 
         // TODO: adjust amount as appropriate based on toIndex_ state
 
@@ -55,7 +83,7 @@ contract AjnaLenderHelper {
         // move the liquidity
         (fromBucketRedeemedLP_, toBucketAwardedLP_, movedAmount_) = pool.moveQuoteToken(amount, fromIndex_, toIndex_, expiry_);
 
-        // transfer remaining LP in fromBucket back to lender
+        // transfer any remaining LP in fromBucket back to lender
         uint256[] memory amounts = new uint256[](1);
         (amounts[0], ) = pool.lenderInfo(fromIndex_, address(this));
         if (amounts[0] != 0) {
@@ -70,8 +98,29 @@ contract AjnaLenderHelper {
         pool.transferLP(address(this), msg.sender, buckets);
     }
 
+    /**
+     *  @notice Pulls quote token from lender into this helper contract
+     */
     function _transferQuoteTokenFrom(address from_, uint256 amount_, IPool pool_) internal {
         uint256 transferAmount = Maths.ceilDiv(amount_, pool_.quoteTokenScale());
         IERC20(pool_.quoteTokenAddress()).safeTransferFrom(from_, address(this), transferAmount);
+    }
+
+    /**
+     *  @notice Converts LP balance to quote token amount, limiting by deposit in bucket
+     *  @return quoteAmount_ The exact amount of quote tokens that can be exchanged for the given `LP`, `WAD` units.
+     */
+    function _lpToQuoteToken(uint256 index_, uint256 lpAmount_, IPool pool_) internal view returns (uint256 quoteAmount_) {
+        (uint256 bucketLP, uint256 bucketCollateral , , uint256 bucketDeposit, ) = pool_.bucketInfo(index_);
+        quoteAmount_ = Buckets.lpToQuoteTokens(
+            bucketCollateral,
+            bucketLP,
+            bucketDeposit,
+            lpAmount_,
+            _priceAt(index_),
+            Math.Rounding.Down
+        );
+
+        if (quoteAmount_ > bucketDeposit) quoteAmount_ = bucketDeposit;
     }
 }
